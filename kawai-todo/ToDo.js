34f06f69ccc88f861a19ea5044d1a6ec31153470
Jiwon Yang <jiwonyang.tech@gmail.com>
2019-08-29 01:57:11 +0900

    Update UI

---
+++ kawai-todo/ToDo.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from "react-native";
+
+const { height, width } = Dimensions.get("window");
+
+export default class ToDo extends Component {
+  state = {
+    isEditing: false,
+    isCompleted: false
+  };
+  render() {
+    const { isCompleted } = this.state.isCompleted;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this._toggleComplete}>
+          <View
+            style={[
+              styles.circle,
+              isCompleted ? styles.CompletedCircle : styles.uncompletedCircle
+            ]}
+          />
+        </TouchableOpacity>
+
+        <Text style={styles.text}> I'm jiwon! </Text>
+      </View>
+    );
+  }
+  _toggleComplete = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted
+      };
+    });
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: width - 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 3,
+    marginRight: 20
+  },
+  CompletedCircle: {
+    borderColor: "#bbb"
+  },
+  uncompletedCircle: {
+    borderColor: "#F23657"
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical: 20
+  }
+});
